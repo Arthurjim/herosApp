134b368e84080f88317a0ae5392961523e5e7dc5
Arthurjim <arthurjim013@gmail.com>
2022-02-02 00:15:57 -0600

    Agregando funcion para recordar la ultima pagina despues del logout

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,10 +1,10 @@
 import React, { useContext } from "react";
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../auth/authContext";
 import { types } from "../../types/types";
 const LoginScreen = () => {
     const { dispatch } = useContext(AuthContext);
-   
+    const navigate = useNavigate();
     
     const handleLogin = () => {
         const action = {
@@ -14,7 +14,8 @@ const LoginScreen = () => {
             },
         };
         dispatch(action);
-      
+        const path = localStorage.getItem('lastPath') || '/';
+        navigate(path, { replace: true });
     };
     return (
         <div className="container mt-5">
--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -1,9 +1,11 @@
 import React, { useContext } from 'react';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 import { AuthContext } from '../auth/authContext';
 
 const PrivateRoute = ({children}) => {
     const {user} = useContext(AuthContext);
+    const {pathname,search} = useLocation();
+    localStorage.setItem('lastPath', pathname+search);
     
   return user.logged ?  (
       <>
