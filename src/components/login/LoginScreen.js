6abac420465aed2b7aa9673f8a5c062a00291477
Arthurjim <arthurjim013@gmail.com>
2022-02-06 00:42:11 -0600

    Agregando input random para username

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -2,15 +2,16 @@ import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../auth/authContext";
 import { types } from "../../types/types";
+import useForm from "../customHooks/useForm";
 const LoginScreen = () => {
     const { dispatch } = useContext(AuthContext);
     const navigate = useNavigate();
-    
+    const [inputValue,handleInput] = useForm({'username':''})
     const handleLogin = () => {
         const action = {
             type: types.login,
             payload: {
-                name: "Arturo",
+                name: inputValue.username,
             },
         };
         dispatch(action);
@@ -21,6 +22,9 @@ const LoginScreen = () => {
         <div className="container mt-5">
             <h1>Login Screen</h1>
             <hr />
+            <div className="my-2">
+                <input type="text" placeholder="Username" onChange={handleInput} name="username" />
+            </div>
             <button className="btn btn-primary" onClick={handleLogin}>
                 Login
             </button>
